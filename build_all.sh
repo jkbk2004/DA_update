8d49e715651068d94d9b057887211ae48d20bcd5
Jong Kim <jong.kim@noaa.gov>
2023-03-21 23:47:37 +0000

    a

---
+++ build_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+source land_mods_hera
+
+for source in add_jedi_incr 
+do 
+cd $source 
+echo 'compiling '$source
+./build.sh 
+cd .. 
+done
+
+# create links 
+echo 'creating jedi links'
+cd jedi/fv3-jedi/Data/
+make_links.sh
